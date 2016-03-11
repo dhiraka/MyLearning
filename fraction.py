beb3d81d6a2f2f47b13faf8c4ba628dd47ff622c
Radhika Andhavarapu <sparkx.radhika@gmail.com>
2016-03-11 15:56:37 +0530

    Add Over ride methods to  - * / < > fractions

--- fraction.py
+++ fraction.py
@@ -19,9 +19,6 @@ class Fraction(object):
     def __str__(self):
         return str(self.num) + "/" + str(self.den)
 
-    def show(self):
-        print(self.num, "/", self.den)
-
     def __add__(self, otherfraction):
 
         newnum = self.num * otherfraction.den + self.den * otherfraction.num
@@ -29,8 +26,44 @@ class Fraction(object):
         gcd_nd = gcd(newnum, newden)
         return Fraction(newnum / gcd_nd, newden / gcd_nd)
 
+    def __sub__(self, otherfraction):
+
+        newnum = self.num * otherfraction.den - self.den * otherfraction.num
+        newden = self.den * otherfraction.den
+        gcd_nd = gcd(newnum, newden)
+        return Fraction(newnum / gcd_nd, newden / gcd_nd)
+
+    def __mul__(self, otherfraction):
+
+        newnum = self.num * otherfraction.num
+        newden = self.den * otherfraction.den
+        gcd_nd = gcd(newnum, newden)
+        return Fraction(newnum / gcd_nd, newden / gcd_nd)
+
+    def __div__(self, otherfraction):
+
+        newnum = self.num * otherfraction.den
+        newden = self.den * otherfraction.num
+        gcd_nd = gcd(newnum, newden)
+        return Fraction(newnum / gcd_nd, newden / gcd_nd)
+
+    def __lt__(self, otherfraction):
+
+        newnum = self.num * otherfraction.den - self.den * otherfraction.num
+        return newnum < 0
+
+    def __gt__(self, otherfraction):
+
+        newnum = self.num * otherfraction.den - self.den * otherfraction.num
+        return newnum > 0
+
 
 x = Fraction(1, 2)
 y = Fraction(2, 3)
 print(x + y)
 print(x == y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x < y)
+print(x > y)
