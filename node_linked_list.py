38c7da63cfb917bba5ee3340862ea1b4ec9dcce3
Radhika Andhavarapu <sparkx.radhika@gmail.com>
2016-03-16 20:58:14 +0530

    Unordered list- Linked list using Nodes

--- node_linked_list.py
+++ node_linked_list.py
@@ -2,6 +2,7 @@ class Node:
     def __init__(self, initdata):
         self.data = initdata
         self.next = None
+        self.position = None
 
     def getData(self):
         return self.data
@@ -14,3 +15,81 @@ class Node:
 
     def setNext(self, newnext):
         self.next = newnext
+
+
+class UnorderedList:
+
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        return self.head is None
+
+    def add(self, item):
+        temp = Node(item)
+        if self.head is not None:
+            temp.setNext(self.head)
+        self.head = temp
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current is not None:
+            count = count + 1
+            current = current.getNext()
+
+        return count
+
+    def search(self, item):
+        current = self.head
+        found = False
+        while current is not None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+
+        return found
+
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        if previous is None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+
+    def append(self, item):
+        temp = Node(item)
+        if self.head is not None:
+            temp.setNext(self.head)
+        self.head = temp
+
+    def insert(self, item):
+        temp = Node(item)
+        if self.head is not None:
+            temp.setNext(self.head)
+        self.head = temp
+
+    def index(self, item):
+        current = self.head
+        found = False
+        while current is not None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+
+        return found
+
+    def pop(self):
+        current = self.head
+        self.head = current.getNext()
